47f408ebccb2f963c2b5d6cd21d6919e5e32edd2
arysttoto <aryst.toto@gmail.com>
2025-07-03 15:55:04 +0500

    Added dependency injection, fixed circular imports problem. Improved config context for routes.py.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,21 @@
 from flask import Flask
-from app.config import Config
-from app.routes import example_blueprint
+from app.config import Config 
 
 
+# Lazy imports are a very good practice in modular Flask applications, don't move them out of the function to avoid circular imports. 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__) 
     app.config.from_object(Config)
 
-    app.register_blueprint(example_blueprint)
+    # Dependency injections, services - AI, Whatsapp client
+    from app.whatsapp import WhatsAppClient
+    from app.ai import AIClient 
 
-    return app
+    app.whatsapp_client = WhatsAppClient(app.config["WHATSAPP_ACCESS_TOKEN"])
+    app.ai_client = AIClient(app.config["OPENAI_API_KEY"])
+
+    # Blueprints/routes creation
+    from app.routes import example_blueprint   
+    app.register_blueprint(example_blueprint)   
+
+    return app 
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,5 @@
 from flask import Blueprint, current_app
-from app.whatsapp import WhatsAppClient
-from app.ai import AIClient
 
-
-# Services
-whatsapp_client = WhatsAppClient(current_app.config["WHATSAPP_ACCESS_TOKEN"])
-ai_client = AIClient(current_app.config["OPENAI_API_KEY"])
 
 # Blueprints
 example_blueprint = Blueprint("example_blueprint", __name__)
