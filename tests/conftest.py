c946730c8b89efdbdbe1a6f72c3b2fcade7c0ae2
arysttoto <aryst.toto@gmail.com>
2025-07-14 09:44:19 +0500

    Whatsapp webhook verification test added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 # Making sure correct root folder is specified, so app package will be seen 
 import pytest
 from app import create_app
+from dotenv import load_dotenv
 
 
+load_dotenv()
 
 @pytest.fixture 
 def client():
--- tests/test_verification_webhook.py
+++ tests/test_verification_webhook.py
@@ -1,4 +1,17 @@
+import os 
+
+
 def test_verification_webhook(client):
     # testing a scenario when webhook verification token is missing
     response = client.get("/")
-    assert response.status_code == 403
+    assert response.status_code == 403 
+
+    # testing when a verification token is provided 
+    hub_challenge = "123456"
+    response = client.get("/", query_string={
+        "hub.mode": "subscribe",
+        "hub.verify_token": os.getenv("WHATSAPP_WEBHOOK_VERIFY_TOKEN", "your-whatsapp-webhook-verify-token"),
+        "hub.challenge": hub_challenge
+    }) 
+    assert response.status_code == 200
+    assert response.data == hub_challenge.encode() 
