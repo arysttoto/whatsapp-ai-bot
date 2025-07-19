7311ffb55ef8e9a8fc37bdc1908a93d6b1008470
arysttoto <aryst.toto@gmail.com>
2025-07-19 12:01:06 +0500

    Message extraction tests passed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,28 @@
 import pytest
 from app import create_app
 from dotenv import load_dotenv
+import os
 
 
 load_dotenv()
+
 
 @pytest.fixture 
 def client():
     app = create_app()
     with app.test_client() as client:
         yield client 
+
+
+@pytest.fixture
+def webhook_credentials():
+
+    hub_challenge = "123456"
+    query_string = {
+        "hub.mode": "subscribe",
+        "hub.verify_token": os.getenv(
+            "WHATSAPP_WEBHOOK_VERIFY_TOKEN", "your-whatsapp-webhook-verify-token"
+        ),
+        "hub.challenge": hub_challenge,
+    }
+    return query_string
--- tests/test_message_extraction.py
+++ tests/test_message_extraction.py
@@ -1,4 +1,3 @@
-import os
 import json
 from pathlib import Path
 
@@ -6,24 +5,46 @@ from pathlib import Path
 # get the resources folder in the tests folder
 resources = Path(__file__).parent / "resources"
 
+# load json templates
+with open((resources / "fake_text_message_update.json"), "r") as json_file:
+    json_fake_message_data = json.load(json_file)
 
-def test_message_extraction(client):
+
+with open((resources / "text_message_update.json"), "r") as json_file:
+    json_message_data = json.load(json_file)
+
+
+with open((resources / "other_update.json"), "r") as json_file:
+    json_other_update_data = json.load(json_file)
+
+
+def test_wrong_message_extraction(client, webhook_credentials):
     # testing when a wrong format json is being sent
-    with open((resources / "fake_text_message_update.json"), "r") as json_file:
-        json_data = json.load(json_file)
-
-    hub_challenge = "123456"
     response = client.post(
         "/",
-        query_string={
-            "hub.mode": "subscribe",
-            "hub.verify_token": os.getenv(
-                "WHATSAPP_WEBHOOK_VERIFY_TOKEN", "your-whatsapp-webhook-verify-token"
-            ),
-            "hub.challenge": hub_challenge,
-        },
-        json=json_data,
+        query_string=webhook_credentials,
+        json=json_fake_message_data,
     )
     # Error 503 should be displayed, json extraction error
     assert response.status_code == 503
-    assert "Error during json extraction: " in str(response.data)
+    assert "Error during json extraction: " in response.get_data(as_text=True)
+
+
+def test_message_extraction(client, webhook_credentials):
+    # Check normal message update handlers
+    response = client.post(
+        "/",
+        query_string=webhook_credentials,
+        json=json_message_data,
+    )
+    assert response.status_code == 200
+
+
+def test_status_update_extraction(client, webhook_credentials):
+    # Check normal status update handlers
+    response = client.post(
+        "/",
+        query_string=webhook_credentials,
+        json=json_other_update_data,
+    )
+    assert response.status_code == 200
